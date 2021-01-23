808fcf9fa93e581e191370b38d1ca254a5769db4
Jaydip <69105601+jaydip1709@users.noreply.github.com>
2021-01-23 17:19:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ API_ID = environ.get('API_ID') # Get these two from https://my.telegram.org eg:1
 API_HASH = environ.get('API_HASH') # Get these two from https://my.telegram.org.  eg:886c1579af458622eb3e20b6558d0337
 TOKEN = environ.get('TOKEN')   ## Get this from @Botfather eg:1381605100:AAHrHbeA370bqjoar2VxC7jc1PM8II6nx8A
 SUDO_USERS = environ.get('SUDO_USERS') # The IDs of the users which can stream, skip, pause and change volume. eg: 1116928578
-GROUP = environ.get('GROUP') # The ID of the group where your bot streams. eg:-1001490179318
+GROUP = environ.get('GROUP') # The ID of the group where your bot streams. eg:-1001232898562
 MONGO_DB_URI = environ.get('MONGO_DB_URI') # Your mongodb uri. eg:mongodb+srv://oxy_1709:yashi1709oxy@cluster0.8gtu0.mongodb.net/test?retryWrites=true&w=majority
 USERS_MUST_JOIN = environ.get('USERS_MUST_JOIN', 'False') # Users must join the group before using the bot (note: the bot should be admin in the group if you enable this)
 LANG = environ.get('LANG', 'en') # Choose the preferred language for your bot. If English leave as it is, or change to the code of any supported language.
